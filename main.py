da17ed81724d171ce900d2bd385a8e5b274ffd59
zan_fulfin_23 <zan@isarlend.com>
2022-12-31 11:38:03 +0100

    Toxicity metrics 2 - plotted average toxicity through time - found top 10 largest and 10 smallest tweets, result seem weird and make no sense

--- main.py
+++ main.py
@@ -5,4 +5,3 @@ if __name__ == "__main__":
     #  last execution of this code took 31061.31 seconds
     download_tweets(tweets_per_day=6000, start_time="01/06/2022 00:00", end_time="28/12/2022 00:00")
     # partition_day_by_tweets(1500, "01/06/2022 00:00", "03/06/2022 00:00")
-
