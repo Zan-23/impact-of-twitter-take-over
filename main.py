b0cd3214ade030637d1838c402e96c2d79ad7f8f
zan_fulfin_23 <zan@isarlend.com>
2023-03-01 13:49:19 +0100

    Small fix

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 from src.twitter_download_functions import download_tweets, partition_day_by_tweets
 
 if __name__ == "__main__":
-    search_query = "(vegan OR #vegan OR vegetarian OR #vegetarian) " \
-                   "lang:en -is:retweet -is:quote -has:links -is:reply"
+    search_query = "( trump OR #trump OR @trump ) lang:en -is:retweet -is:quote -has:links -is:reply"
     #  last execution of this code took 31061.31 seconds
     download_tweets(search_query=search_query,
-                    tweets_per_day=9500,
+                    tweets_per_day=18000,
                     start_time="01/06/2022 00:00",
                     end_time="03/01/2023 00:00")
     # partition_day_by_tweets(1500, "01/06/2022 00:00", "03/06/2022 00:00")
